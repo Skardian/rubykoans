2269d4303969742c67441dcf82fc178375a29ff1
Pepe Padial <jose.carlos.padial@gmail.com>
2016-07-25 15:53:41 +0200

    Complete koans up to second 'triangle' koan

--- triangle.rb
+++ triangle.rb
@@ -14,6 +14,10 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  a, b, c = [a,b,c].sort
+  raise TriangleError if a == 0
+  raise TriangleError if a + b <= c
+
   return :equilateral if a == b && b == c
   return :scalene if a != b && a != c && b != c
   :isosceles
